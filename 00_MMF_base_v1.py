d67a5248fa9c9a5810e304a8983859fedb16a2ee
RyanGriffith <griffi.ryan@gmail.com>
2021-07-05 09:52:52 +1200

    base v1

---
+++ 00_MMF_base_v1.py
@@ -0,0 +1,25 @@
+# import statements
+
+# functions go here
+
+# *** main routine ***
+
+# set up dictionaries / lists needed to hold data
+
+# ask user if they have used the program before
+
+# loop to get ticket details
+
+    # get name (can't be blank)
+
+    # get age (between 11 and 130)
+
+    # loop to ask for snacks
+
+    # calculate snacks price
+
+    # ask for payment method
+
+# calculate total sales and profit
+
+# output data to text file
