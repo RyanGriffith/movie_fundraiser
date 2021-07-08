e7fdc93cd2bba9a9faece4b71c94d6f8f3f3788e
RyanGriffith <griffi.ryan@gmail.com>
2021-07-08 12:55:26 +1200

    not blank function v1

---
+++ 02_not_blank.py
@@ -0,0 +1,15 @@
+# Functions go here
+
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response != "":
+            return response
+        else:
+            print("Sorry - this can't be blank")
+
+# main routine
+name = not_blank("Name: ")
