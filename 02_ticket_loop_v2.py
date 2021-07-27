e788767b76b948f9201d210b0dd5c9882c5edb9c
RyanGriffith <griffi.ryan@gmail.com>
2021-07-28 10:25:33 +1200

    ticket loop v2

--- 02_ticket_loop_v2.py
+++ 02_ticket_loop_v2.py
@@ -3,7 +3,7 @@
 # initialise loop so that it runs at least once
 name = " "
 count = 0
-MAX_TICKETS = 50
+MAX_TICKETS = 5
 
 while name != "xxx" and count < MAX_TICKETS:
     print("You have {} seats left ".format(MAX_TICKETS - count))
