671cbc4aa100663841cec4df6b2e39c020e0f73a
RyanGriffith <griffi.ryan@gmail.com>
2021-07-30 14:04:50 +1200

    base component v1

--- 00_base_component_v1.py
+++ 00_base_component_v1.py
@@ -1,16 +1,7 @@
 # import statements
 
 # functions go here
-def not_blank(question):
-    valid = False
 
-    while not valid:
-        response = input(question)
-
-        if response != "":
-            return response
-        else:
-            print("Sorry - this can't be blank")
 # *** main routine ***
 
 # set up dictionaries / lists needed to hold data
@@ -20,27 +11,7 @@ def not_blank(question):
 # loop to get ticket details
 
     # get name (can't be blank)
-name = not_blank("Name: ")
-name = " "
-count = 0
-MAX_TICKETS = 5
 
-while name != "xxx" and count < MAX_TICKETS:
-    print("You have {} seats left ".format(MAX_TICKETS - count))
-
-    # get details
-    name = input("name: ")
-    count += 1
-    print()
-    if name == "xxx":
-        count -= 1
-    if name == " ":
-        print(error_message)
-
-if count == MAX_TICKETS:
-    print("You have sold all available tickets!")
-else:
-    print("You have sold {} tickets. There are still {} places available".format(count, MAX_TICKETS - count))
     # get age (between 11 and 130)
 
     # loop to ask for snacks
