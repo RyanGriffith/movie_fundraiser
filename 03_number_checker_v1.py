c14a5ec9229b2d8a657e32d55bbc0f5fa0ca3c8b
RyanGriffith <griffi.ryan@gmail.com>
2021-08-02 09:41:29 +1200

    03_number_checker_v1

---
+++ 03_number_checker_v1.py
@@ -0,0 +1,17 @@
+# function goes here
+
+def int_check(question, low_num, high_num):
+
+    error = "Please enter a whole number between {} and {}".format(low_num, high_num)
+
+    valid = false
+    while not valid:
+
+        try:
+            response = int(input(question))
+            return response
+
+        except ValueError:
+            print(error)
+
+# main routine goes here
