ea71d4527501e07b357990e848c5d5166e59ce2c
RyanGriffith <griffi.ryan@gmail.com>
2021-09-10 13:57:57 +1200

    snack checker (semi working)

--- 06_Snack_Checker.py
+++ 06_Snack_Checker.py
@@ -1,9 +1,9 @@
 # valid snacks hold list of all valid snacks
 valid_snacks = [
-    ['Popcorn', 'p', 'corn', 'a'],
+    ['Popcorn', 'popcorn', 'p', 'corn', 'a'],
     ['M&Ms', 'mms', 'MMS', 'm', 'b'],
-    ['Pita Chips', 'pc', 'chips', 'c'],
-    ['Water', 'w', 'd']
+    ['Pita Chips', 'pita chips', 'pc', 'chips', 'c'],
+    ['Water', 'w', 'd', 'water', 'liquid']
 ]
 
 # initialise variables
@@ -11,15 +11,15 @@ snack_ok = ""
 snack = ""
 
 # loop 3 times for fast testing
-for item in range(0, 3):
+for item in range(0, 1):
 
     # asks user for desired snack
-    desired_snack = input("Snack: ").lower()
+    desired_snack = input("Snack: ")
 
     for var_list in valid_snacks:
 
         # if the snack is in the list
-        if desired_snack in var_list:
+        if desired_snack in var_list[0]:
 
             # get full name of snack
             # in title case
@@ -29,8 +29,9 @@ for item in range(0, 3):
         # if the chosen snack isn't valid
         else:
             snack_ok = "no"
+            break
     # if snack not ok - ask again
-    if snack_ok == "yes:":
+    if snack_ok == "yes":
         print("snack choice: ", snack)
     else:
         print("invalid snack")
